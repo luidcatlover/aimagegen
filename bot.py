56884892e428c2e1ddd5a8fda23bac2f307f0a87
luid <timbitkin@gmail.com>
2025-02-16 18:05:29 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,38 @@
+import telebot
+from telebot import types
+from logic import Text2ImageAPI
+from config import TOKEN, SECRET_TOKEN, API_TOKEN
+import os
+
+
+bot = telebot.TeleBot(token=TOKEN)
+
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.send_message(message.chat.id, 'Привет! Я бот, который генерирует изображение по запросу. Введи /generate и запрос, по которому будет сгенерировано изображение.')
+
+@bot.message_handler(commands=['generate'])
+def generate_image(message):
+    try:
+        promt = message.text.replace('/generate ', '')
+        msg = bot.reply_to(message, 'Генерация изображения..')
+
+        api = Text2ImageAPI('https://api-key.fusionbrain.ai/', API_TOKEN, SECRET_TOKEN)
+        model_id = api.get_model()
+        uuid = api.generate(promt, model_id)
+        images = api.check_generation(uuid)[0]
+
+        api.save_image(images, 'result.jpg')
+
+        bot.delete_message(msg.chat.id, msg.message_id)
+
+        with open('result.jpg', 'rb') as photo:
+            bot.send_photo(message.chat.id, photo=photo)
+
+        os.remove('result.jpg')
+    except Exception as err:
+        bot.send_message(message.chat.id, f'Возникла ошибка при генерации изображения: {err}')
+
+bot.infinity_polling()
